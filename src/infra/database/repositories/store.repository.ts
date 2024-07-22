15151776a1789173d5c66c98c003d4b09700aa35
meliodas0001 <riquemorozinee@gmail.com>
2024-07-21 23:10:26 -0300

    feat: store repository implement

--- src/infra/database/repositories/store.repository.ts
+++ src/infra/database/repositories/store.repository.ts
@@ -1,20 +1,28 @@
-import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
 
 import { StoreEntity } from '@database/entities/store.entity';
+
 import { IStoreRepository } from '@domains/repositories/IStoreRepository';
 import { IStoreCreateDTO } from '@domains/dtos/store/IStoreCreateDTO';
 
 @Injectable()
 export class StoreRepository implements IStoreRepository {
   constructor(
-    @InjectRepository(StoreEntity) storeEntity: Repository<StoreEntity>,
+    @InjectRepository(StoreEntity) private storeEntity: Repository<StoreEntity>,
   ) {}
-  create(store: IStoreCreateDTO): Promise<void> {
-    throw new Error('Method not implemented.');
+  async create(store: IStoreCreateDTO): Promise<void> {
+    const storeCreated = this.storeEntity.create(store);
+
+    await this.storeEntity.save(storeCreated);
   }
-  findStoreById(id: string): Promise<StoreEntity> {
-    throw new Error('Method not implemented.');
+
+  async findStoreById(id: string): Promise<StoreEntity> {
+    return await this.storeEntity.findOne({
+      where: {
+        id,
+      },
+    });
   }
 }
