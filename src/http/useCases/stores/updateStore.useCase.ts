272b12266365f0ffc2e9608a58c1a8c0f1d20f43
meliodas0001 <riquemorozinee@gmail.com>
2024-07-24 20:18:20 -0300

    feat: updateStore useCases

--- src/http/useCases/stores/updateStore.useCase.ts
+++ src/http/useCases/stores/updateStore.useCase.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 
 import { IStoreRepository } from '@domains/repositories/IStoreRepository';
+import { IStoreUpdate } from '@domains/dtos/store/IStoreUpdate';
 
 @Injectable()
 export class UpdateStoreUseCase {
   constructor(private storeRepository: IStoreRepository) {}
+
+  async execute(store: IStoreUpdate): Promise<void> {
+    const storeFind = await this.storeRepository.findStoreById(store.storeId);
+
+    if (!storeFind) throw new UnauthorizedException('Store not found');
+
+    await this.storeRepository.updateStore(store);
+  }
 }
